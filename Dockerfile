28db242e56ae921374a3730a0d1bbab9f29ec470
Bhushan Rane <eulers_identity@Bhushans-MacBook-Air.local>
2022-08-23 23:36:07 +0530

    Updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.9
 
 ADD . /app
-
+RUN python3 -m pip install --upgrade pip
 RUN pip3 install -r /app/requirements.txt
 
 CMD ["python3", "/app/main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
