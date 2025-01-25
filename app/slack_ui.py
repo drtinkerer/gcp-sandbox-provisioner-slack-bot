bb1cbb6f527a096c5b4b4058eb5c29e2582b1b42
Bhushan Rane <b.rane@etops.ch>
2025-01-26 00:25:34 +0530

    updated path

--- app/slack_ui.py
+++ app/slack_ui.py
@@ -6,7 +6,7 @@ team_folders = config.AUTHORIZED_TEAM_FOLDERS
 
 class BlockTemplates:
     def __init__(self):
-        with open('slack_ui_blocks/sandbox_request_form.json', 'r') as file:
+        with open('app/slack_ui_blocks/sandbox_request_form.json', 'r') as file:
             sandbox_request_form = json.load(file)
             sandbox_request_form["callback_id"] = "sandbox_request_form"
             team_name_options = [{'text': {'type': 'plain_text', 'text': team, 'emoji': True},'value': team} for team in team_folders]
@@ -16,5 +16,5 @@ class BlockTemplates:
                     block["element"]["options"] = team_name_options
             
         self.sandbox_request_form = sandbox_request_form
-        with open('slack_ui_blocks/app_home.json', 'r') as file:
+        with open('app/slack_ui_blocks/app_home.json', 'r') as file:
             self.app_home = json.load(file)
